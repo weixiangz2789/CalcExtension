719f486107956e85fd917be284d8c1c136dfb094
1E10 Laptop <sample@bths.edu>
2022-10-26 11:01:27 -0400

    w

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    private double num1;
+    private double num2;
+    private String operation;
+
+    public Calculator(double num1, double num2, String operation){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operation = operation;
+    }
+
+    public double calculate(){
+        if (operation.equals("*")){
+            return num1 * num2;
+        }
+        if (operation.equals("/")){
+            return num1 / num2;
+        }
+        if (operation.equals("+")){
+            return num1 + num2;
+        }
+        if (operation.equals("-")){
+            return num1 - num2;
+        }
+        if (operation.equals("%")){
+            return num1 % num2;
+        }
+        if (operation.equals("^")){
+            return Math.pow(num1, num2);
+        }
+        return 0;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter an expression with *, /, +, -, %, or ^: ");
+        String expression = scan.nextLine();
+        int index;
+        if (expression.indexOf("*") != -1){
+            index = (expression.indexOf("*");
+        }
+        if (expression.indexOf("/") != -1){
+            index = (expression.indexOf("/");
+        }
+        if (expression.indexOf("+") != -1){
+            index = (expression.indexOf("+");
+        }
+        if (expression.indexOf("-") != -1){
+            index = (expression.indexOf("-");
+        }
+        if (expression.indexOf("%") != -1){
+            index = (expression.indexOf("%");
+        }
+        if (expression.indexOf("^") != -1){
+            index = (expression.indexOf("^");
+        }
+        double num1 = Integer.parseInt(expression.substring(0, index-1));
+        double num2 = Integer.parseInt(expression.substring(index + 1, expression.length()));
+    }
 }
