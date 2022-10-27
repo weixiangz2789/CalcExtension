263d4f16fe3917b16deccc2370e011c4fb0cc2d4
1E10 Laptop <sample@bths.edu>
2022-10-27 11:00:24 -0400

    w

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,39 @@ public class Main {
 
         System.out.println("Enter an expression with *, /, +, -, %, or ^: ");
         String expression = scan.nextLine();
-        int index;
-        if (expression.indexOf("*") != -1){
-            index = (expression.indexOf("*");
+        int index = 0;
+        String operation = "";
+        if (expression.indexOf("*") != -1) {
+            index = expression.indexOf("*");
+            operation = "*";
         }
         if (expression.indexOf("/") != -1){
-            index = (expression.indexOf("/");
+            index = expression.indexOf("/");
+            operation = "/";
         }
         if (expression.indexOf("+") != -1){
-            index = (expression.indexOf("+");
+            index = expression.indexOf("+");
+            operation = "+";
         }
         if (expression.indexOf("-") != -1){
-            index = (expression.indexOf("-");
+            index = expression.indexOf("-");
+            operation = "-";
         }
         if (expression.indexOf("%") != -1){
-            index = (expression.indexOf("%");
+            index = expression.indexOf("%");
+            operation = "&";
         }
         if (expression.indexOf("^") != -1){
-            index = (expression.indexOf("^");
+            index = expression.indexOf("^");
+            operation = "^";
         }
         double num1 = Integer.parseInt(expression.substring(0, index-1));
         double num2 = Integer.parseInt(expression.substring(index + 1, expression.length()));
+
+        Calculator calc = new Calculator(num1, num2, operation);
+        calc.calculate();
+
+
+
     }
 }
